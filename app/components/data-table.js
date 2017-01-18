45c0dbe84a8a421e09e855d362fe8c5dd69e0772
Rajiv Chauhan <rajiv.chauhan@gmail.com>
2017-01-17 23:47:05 -0500

    Added pagination, indexing, and button toolbars

--- app/components/data-table.js
+++ app/components/data-table.js
@@ -13,16 +13,19 @@ export default Ember.Component.extend({
 
     sortedData: Ember.computed.sort('data', 'sortBy'),
 
-    filteredResults: Ember.computed('data', 'sortedData', 'filterValue', 'filterBy', function() {
+    filteredResults: Ember.computed('data', 'sortedData', 'filterValue', 'filterBy', 'currentPage', function() {
         let data = this.get('sortedData');
         if(data === undefined) {
             data = this.get('data');
         }
 
+        let currentPage = this.get('currentPage');
+        let pageSize = this.get('pageSize');
+
         let filterValue = this.get('filterValue');
         let filterBy = this.get('filterBy');
         if(typeof filterValue === 'undefined' && typeof filterValue === 'undefined') {
-            return data;
+            return data.slice((currentPage - 1) *pageSize, currentPage*pageSize);
         } else {
             return data.filter((item) => {
                 let itemValue = Ember.get(item, filterBy);
@@ -31,11 +34,48 @@ export default Ember.Component.extend({
                 } else {
                     return itemValue.match(filterValue);
                 }
-            });
+            }).slice((currentPage - 1) * pageSize, currentPage*pageSize);
         }
     }),
 
     ascending: false,
+
+    pageSize: 10,
+
+    currentPage: 1,
+
+    pagesToDisplayFrom: 1,
+
+    noOfPagesToDisplay: 5,
+
+    pageDisplaySize: Ember.computed('noOfPagesToDisplay', 'totalPages', function(){
+        let pageItemSize = this.get('noOfPagesToDisplay');
+        let totalPages = this.get('totalPages');
+        let array = new Array(parseInt(Math.min(totalPages, pageItemSize)));
+        return array;
+    }),
+
+    endPageToDisplay: Ember.computed('pagesToDisplayFrom', 'noOfPagesToDisplay', 'totalPages', function(){
+        return Math.min(this.get('totalPages'), this.get('pagesToDisplayFrom') + this.get('noOfPagesToDisplay') - 1);
+    }),    
+
+    totalPages: Ember.computed('filteredResults', 'filterValue', 'pageSize', function() {
+        let dataSet = this.get('filteredValue') === undefined ? 'data.length' :  'filteredResults.length';
+        let dataLength = this.get(dataSet);
+        let pageSize = this.get('pageSize');
+        return Math.ceil(dataLength / pageSize);
+    }),
+
+    isFirstPage: Ember.computed('currentPage', function(){
+        return this.get('currentPage') === 1;
+    }),
+
+    isLastPage: Ember.computed('currentPage', 'totalPages', function(){
+        let currentPage = this.get('currentPage');
+        let totalPage = this.get('totalPages');
+        return currentPage === totalPage;
+    }),
+
 
     sortBy: Ember.computed('sortingKey', 'ascending', function(){
         const sortKey = this.get('sortingKey');
@@ -72,6 +112,24 @@ export default Ember.Component.extend({
         }
     },
     actions: {
+        gotoPage(pageIndex) {
+            Logger.info(`Going to page ${pageIndex}`);
+            let currentPage = this.get('currentPage');
+            let endPageToDisplay = this.get('endPageToDisplay');
+            let pagesToDisplayFrom = this.get('pagesToDisplayFrom');
+            if(currentPage === endPageToDisplay && pageIndex > currentPage) {
+                let totalPages = this.get('totalPages');
+                let noOfPagesToDisplay = this.get('noOfPagesToDisplay');
+                if(endPageToDisplay + noOfPagesToDisplay >= totalPages) {
+                    this.set('pagesToDisplayFrom', totalPages - noOfPagesToDisplay + 1);
+                } else {
+                    this.set('pagesToDisplayFrom', pageIndex);
+                }
+            } else if(currentPage === pagesToDisplayFrom && pageIndex < currentPage) {
+                this.set('pagesToDisplayFrom', pageIndex);
+            }
+            this.set('currentPage', pageIndex);
+        },
         filterData(filterValue, columnName) {
             this.set('filterBy', columnName);
         },
